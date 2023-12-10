3d7ed68bcdf1b9a99dbce1e5b3ffa646b86f5c02
Rey <johnreydado3@gmail.com>
2023-12-10 23:10:26 +0800

    story reaction

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
@@ -7,13 +7,52 @@ import { AuthService } from '../auth.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class SignInComponent {
-  email: string;
+export class SignInComponent implements AfterViewInit {
+  @ViewChild('videoElement') videoElement!: ElementRef;  email: string;
   password: string;
 
   constructor(private authService: AuthService, private router: Router) {
     this.email = '';
     this.password = '';
+  }
+
+  ngAfterViewInit() {
+  }
+
+  startCamera() {
+    if (this.videoElement.nativeElement.srcObject) {
+      this.stopCamera();
+    } else {
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+          this.videoElement.nativeElement.srcObject = stream;
+          this.videoElement.nativeElement.play();
+        });
+      }
+    }
+  }
+
+  stopCamera() {
+    const stream = this.videoElement.nativeElement.srcObject;
+    const tracks = stream.getTracks();
+  
+    tracks.forEach(function(track: MediaStreamTrack) {
+      track.stop();
+    });
+  
+    this.videoElement.nativeElement.srcObject = null;
+  }
+
+  capture() {
+    const canvas = document.createElement('canvas');
+    canvas.width = this.videoElement.nativeElement.videoWidth;
+    canvas.height = this.videoElement.nativeElement.videoHeight;
+    const context = canvas.getContext('2d');
+    if (context) {
+      context.drawImage(this.videoElement.nativeElement, 0, 0);
+    }
+    const image = canvas.toDataURL('image/png');
+    // Now you have the image captured from the webcam. You can send this image to your facial recognition service.
   }
 
   signIn(event: Event) {
